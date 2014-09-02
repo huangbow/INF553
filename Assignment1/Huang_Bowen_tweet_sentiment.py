54bc7b8e93654013114930117338538e48ed6195
huangbowen <Alex@huangbowendeMacBook-Pro.local>
2014-09-02 00:15:12 -0700

    still have problem with unicode
    UnicodeEncodeError: 'ascii' codec can't encode character

--- Assignment1/Huang_Bowen_tweet_sentiment.py
+++ Assignment1/Huang_Bowen_tweet_sentiment.py
@@ -24,19 +24,22 @@ def main():
         num_sum=0
         #json-->dictionary
         decode=json.loads(line)
+        # decode1=json.dumps(line)
         #print decode
         #dict ergodic
-        # print v
         for key,val in scores.items():#words_file
             for k,v in decode.items():#twitter_file
+                
                 v_word=str(v).split(' ')#split sentence into words
                 # print v_word
                 for word in v_word:    
                     str_exist=str(word).find(key)#whether words in twitter_file contains any words
                     if str_exist>-1:
-                        num_sum+=int(val)
+                        if len(str(word))-len(key)<=2:#make sure the word is exactly the word, rather than a slice
+                            num_sum+=int(val)
+                            # print word
+                            # print v_word
+                            # print key
         print num_sum
-
-
 if __name__ == '__main__':
     main()
