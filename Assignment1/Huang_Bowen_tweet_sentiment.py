07c24fea0766e6303e93f3c692decd814a7cca74
huangbowen <Alex@huangbowendeMacBook-Pro.local>
2014-09-02 17:10:46 -0700

    Problem worked out!
    the only thing I should take care of is the value of “text”

--- Assignment1/Huang_Bowen_tweet_sentiment.py
+++ Assignment1/Huang_Bowen_tweet_sentiment.py
@@ -27,19 +27,28 @@ def main():
         # decode1=json.dumps(line)
         #print decode
         #dict ergodic
-        for key,val in scores.items():#words_file
-            for k,v in decode.items():#twitter_file
-                
-                v_word=str(v).split(' ')#split sentence into words
-                # print v_word
-                for word in v_word:    
-                    str_exist=str(word).find(key)#whether words in twitter_file contains any words
-                    if str_exist>-1:
-                        if len(str(word))-len(key)<=2:#make sure the word is exactly the word, rather than a slice
-                            num_sum+=int(val)
-                            # print word
-                            # print v_word
-                            # print key
+        # for key,val in scores.items():#words_file
+        #     for k,v in decode.items():#twitter_file
+        #         v_word=str(v).split(' ')#split sentence into words
+        #         # print v_word
+        #         for word in v_word:    
+        #             str_exist=str(word).find(key)#whether words in twitter_file contains any words
+        #             if str_exist>-1:
+        #                 if len(str(word))-len(key)<=2:#make sure the word is exactly the word, rather than a slice
+        #                     num_sum+=int(val)
+        #                     # print word
+        #                     # print v_word
+        #                     # print key
+        # for k,v in decode.items():
+        #     print k
+        v_word=decode["text"].split()
+        for key,val in scores.items():
+            for word in v_word:
+                word=word.lower()
+                uni_word = word.encode('utf-8').translate(None, string.punctuation)#Remove ASCII punctuation from your Unicode string
+                if uni_word.find(key)>-1:
+                    if len(uni_word)-len(key)<1:#uni_word==key
+                        num_sum+=int(val)
         print num_sum
 if __name__ == '__main__':
     main()
