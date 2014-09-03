9d933de2ecbd2c2895b7b66a99f765a9805e41e2
huangbowen <Alex@huangbowendeMacBook-Pro.local>
2014-09-02 18:29:55 -0700

    Problem 3 works out
     but should censor the LANGUAGE

---
+++ Assignment1/Huang_Bowen_frequency.py
@@ -0,0 +1,51 @@
+from __future__ import division
+import sys
+import json
+import string
+
+
+def main():
+	tweet_file=open(sys.argv[1])
+	term_in_tweet={}
+	term_freq={}
+
+	#get each term
+	for line in tweet_file:
+		decode=json.loads(line)
+		v_word=decode["text"].split()
+		for word in v_word:
+			word=word.lower()
+			word=word.encode('utf-8').translate(None, string.punctuation)
+			if term_in_tweet.get(word)!=None:
+				term_in_tweet[word]+=1#count the term
+			else:
+				term_in_tweet[word]=1#first time get this term
+	#print term_in_tweet.items()
+	#end-----get each term
+
+
+	num_of_words=0
+	num_of_terms=0
+
+	for k,v in term_in_tweet.items():
+		num_of_words+=int(v)
+		num_of_terms+=1
+	for k,v in term_in_tweet.items():
+		num_up=v/num_of_words
+		#num_down=v/num_of_terms
+		#print num_down
+
+		#an interesting way to count freq
+		#print num_up/num_down
+
+		#the assign way
+		term_freq[k]=num_up
+
+
+	#print term_freq.items()
+	for k,v in term_freq.items():
+		print str(k)+"\t"+str(v)
+
+
+if __name__ == '__main__':
+    main()
--- Assignment1/Huang_Bowen_tweet_sentiment.py
+++ Assignment1/Huang_Bowen_tweet_sentiment.py
@@ -20,28 +20,10 @@ def main():
 
     #tweet_file
     for line in tweet_file:
-        #newstring = line.encode('utf-8').translate(None, string.punctuation)
         num_sum=0
         #json-->dictionary
         decode=json.loads(line)
-        # decode1=json.dumps(line)
-        #print decode
-        #dict ergodic
-        # for key,val in scores.items():#words_file
-        #     for k,v in decode.items():#twitter_file
-        #         v_word=str(v).split(' ')#split sentence into words
-        #         # print v_word
-        #         for word in v_word:    
-        #             str_exist=str(word).find(key)#whether words in twitter_file contains any words
-        #             if str_exist>-1:
-        #                 if len(str(word))-len(key)<=2:#make sure the word is exactly the word, rather than a slice
-        #                     num_sum+=int(val)
-        #                     # print word
-        #                     # print v_word
-        #                     # print key
-        # for k,v in decode.items():
-        #     print k
-        v_word=decode["text"].split()
+        v_word=decode["text"].split()#find the value of key"text"
         for key,val in scores.items():
             for word in v_word:
                 word=word.lower()
