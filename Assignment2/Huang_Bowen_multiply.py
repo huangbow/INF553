a2fd0513c4574b48c82fe7a8861a07277f1deea1
huangbowen <Alex@huangbowendeMacBook-Pro.local>
2014-09-22 17:28:44 -0700

    start problem 5

--- Assignment2/Huang_Bowen_multiply.py
+++ Assignment2/Huang_Bowen_multiply.py
@@ -5,17 +5,14 @@ mr=MapReduce.MapReduce()
 
 def mapper(record):
 	key=record[0]
-	value=record[1]
-	h=hash(value)
-	#remove last 10 letters
-	value=value[:-10]
-	mr.emit_intermediate(h,value)
-
+	x=record[1]
+	y=record[2]
+	val=record[3]
+	h=hash(x)+hash(y)
+	mr.emit_intermediate(h,(x,y,val))
 
 def reducer(key,list_of_value):
-	#guarantee that if it duplicates, only output one
-	mr.emit(list_of_value[0])
-
+	
 
 if __name__ == '__main__':
 	inputdata=open(sys.argv[1])
